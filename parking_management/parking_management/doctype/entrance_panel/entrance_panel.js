f1768896373e8c686c604171b13e197a0e07c4ec
AryamanRAWAT <arawat.kol@gmail.com>
2024-08-30 16:23:27 +0530

    Add doc types # Implement 2

---
+++ parking_management/parking_management/doctype/entrance_panel/entrance_panel.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Aryaman Rawat and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Entrance Panel", {
+// 	refresh(frm) {
+
+// 	},
+// });
